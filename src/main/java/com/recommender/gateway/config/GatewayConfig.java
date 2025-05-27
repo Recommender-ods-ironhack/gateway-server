57f7a7b3331dd5092ac67702272c76fed3771a88
Gab Caser <caserogabriela@gmail.com>
2025-05-27 11:58:42 +0200

    get item by id

--- src/main/java/com/recommender/gateway/config/GatewayConfig.java
+++ src/main/java/com/recommender/gateway/config/GatewayConfig.java
@@ -1,4 +1,18 @@
 package com.recommender.gateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class GatewayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("recommender", r-> r
+                        .path("/api/get/item/**")
+                        .uri("lb://recommender-service"))
+                .build();
+    }
 }
